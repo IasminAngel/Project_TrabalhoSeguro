d58046786fbe57aa53ab9154fb5c783dc50ad321
Ermenz <143419782+Ermenz@users.noreply.github.com>
2025-03-10 14:30:57 -0300

    Mudanças
    alteração dos app.py

--- public/src/utils/app_final.py
+++ public/src/utils/app_final.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     botoes = [
-        {"href": "/funcionarios", "src": "/public/src/assets/imgs/Funcionario.png", "alt": "Funcionários"},
-        {"href": "/setores", "src": "/public/src/assets/imgs/Setores.png", "alt": "Setores"},
-        {"href": "/registros", "src": "/public/src/assets/imgs/Registros.png", "alt": "Registros"}
+        {"href": "/funcionarios", "src": "/static/src/assets/imgs/Funcionario.png", "alt": "Funcionários"},
+        {"href": "/setores", "src": "/static/src/assets/imgs/Setores.png", "alt": "Setores"},
+        {"href": "/registros", "src": "/static/src/assets/imgs/Registros.png", "alt": "Registros"}
     ]
     icones_principais = [
         {"href": "#", "class": "fas fa-download", "extra_class": "download-icon"},
@@ -18,6 +18,18 @@ def index():
         {"href": "#", "class": "fas fa-user", "extra_class": "person-icon"}
     ]
     return render_template('index.html', botoes=botoes, icones_principais=icones_principais, icones_top_right=icones_top_right)
+
+@app.route('/setores')
+def setores():
+    return render_template('setores.html')  # Template da página de Setores
+
+@app.route('/registros')
+def registros():
+    return render_template('registros.html')  # Template da página de Registros
+
+@app.route('/funcionarios')
+def funcionarios():
+    return render_template('funcionarios.html')  # Template da página de Funcionários
 
 if __name__ == '__main__':
     app.run(debug=True)
