0b66617c8e765fbac4af6de37ac7a09d192bb5ce
Iasmin Angel <143419093+IasminAngel@users.noreply.github.com>
2025-03-10 22:27:02 -0300

    Tentativa falha de inserção - Conexão feita !

--- backend/database/node/server.js
+++ backend/database/node/server.js
@@ -1,22 +1,42 @@
 import express from "express";
-import mysql from 'mysql';
+import con from "./connection.js";
 
-const con = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'trabalho_seguro'
+const app = express();
+
+// Middleware para processar JSON
+app.use(express.json())
+
+app.get("/", (req, res) => {
+  con.query("SELECT * FROM funcionario", (err, result) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(result);
+    }
+  });
 });
 
-const app = new express();
+/*app.post("/inserir", (req, res) => {
+  const { opinionField, upgradeField } = req.body;
+  console.log("Recebido:", opinionField, upgradeField);
+  
+  if (!opinionField || !upgradeField) {
+    return res.status(400).send("Todos os campos são obrigatórios!");
+  }
 
-app.get('/', (req, res) => {
-    // Executamos a query para o banco de dados
-    con.query('SELECT * FROM funcionario', (err, result) => {
-        res.send(result);
-    })
-})
+  const sql = "INSERT INTO avaliacao_site (opiniao, melhoras) VALUES (?, ?)";
+  
+  con.query(sql, [opinionField, upgradeField], (err, result) => {
+    if (err) {
+      console.error("Erro ao inserir:", err); 
+      res.status(500).send("Erro ao inserir dados");
+    } else {
+      console.log("Dados inseridos com sucesso!"); 
+      res.send("Dados inseridos com sucesso!");
+    }
+  });
+});*/
 
-app.listen('3030', () => {
-    console.log('Running Server');
-})
+app.listen(3030, () => {
+  console.log("Running Server on port 3030");
+});
