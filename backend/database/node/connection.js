c5980afb0af35645bfe0dba9d06f7269da762599
Iasmin Angel <143419093+IasminAngel@users.noreply.github.com>
2025-03-10 22:27:02 -0300

    Tentativa falha de inserção - Conexão feita !

---
+++ backend/database/node/connection.js
@@ -0,0 +1,18 @@
+import mysql from "mysql";
+
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "trabalho_seguro",
+});
+
+con.connect((err) => {
+  if (err) {
+    console.error("Erro ao conectar ao banco de dados:", err);
+  } else {
+    console.log("Conectado ao banco de dados!");
+  }
+});
+
+export default con;
