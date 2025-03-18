d945f776a5493664a4dad3de0e6062da53167762
Iasmin Angel <143419093+IasminAngel@users.noreply.github.com>
2025-03-18 14:06:24 +0000

    Novas att conect crud acidentes + method pay + stripe

--- backend/database/node/server.js
+++ backend/database/node/server.js
@@ -46,6 +46,26 @@ app.post('/salvar-avaliacao', (req, res) => {
   });
 });
 
+app.post('/registrar-acidente', (req, res) => {
+  const { details } = req.body;
+
+  if (!estrela || !opiniao) {
+      return res.status(400).json({ error: 'Avaliação e opinião são obrigatórios!' });
+  }
+
+  const query = 'INSERT INTO Avaliacao_site (estrela, opiniao, melhoras) VALUES (?, ?, ?)';
+  connection.query(query, [estrela, opiniao, melhoras], (err, results) => {
+      if (err) {
+          console.error('Erro ao salvar dados:', err);
+          return res.status(500).json({ error: 'Erro ao salvar dados no banco de dados.' });
+      }
+
+      res.status(201).json({ message: 'Avaliação salva com sucesso!', id: results.insertId });
+  });
+});
+
+
+/* --------------- */
 
 app.post('/salvar-avaliacao', (req, res) => {
   const { estrela, opiniao, melhoras } = req.body;
