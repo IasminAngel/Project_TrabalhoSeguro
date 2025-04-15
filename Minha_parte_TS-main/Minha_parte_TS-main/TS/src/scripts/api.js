22b98a047aa9f1d6ba8d79dddb25f1fe19b1cebd
Marcoslsilva <112900211+Marcoslsilva@users.noreply.github.com>
2025-04-15 10:33:57 -0300

    Add files via upload

---
+++ Minha_parte_TS-main/Minha_parte_TS-main/TS/src/scripts/api.js
@@ -0,0 +1,31 @@
+
+// import { exp_inp_func } from "./Adicionar_setor.js";
+import { exp_tecnico_value } from "./Adicionar_setor.js";
+
+
+
+export function expFuncionarios() {
+
+    let funcionarios = [
+        {
+            id : 1,
+            nome : "Maria Angelina das Graças",
+            tecnico : exp_tecnico_value,
+            funcao : "ss"
+        },
+        
+        {
+            id : 2,
+            nome : "Joana dos Santos Almeida",
+            tecnico : exp_tecnico_value,
+            funcao : "Gerenciar a equipe de vendas"
+        }
+    
+    ]
+
+    return funcionarios;
+}
+
+
+
+
